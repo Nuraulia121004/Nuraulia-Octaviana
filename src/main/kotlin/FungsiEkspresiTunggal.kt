ba1dd726ac6a2254d6ff904385d4e39074831cc4
Nuraulia Octaviana <nurauliaaa12@gmail.com>
2024-09-12 22:12:42 +0700

    Fungsi Ekspresi Tunggal

---
+++ src/main/kotlin/FungsiEkspresiTunggal.kt
@@ -0,0 +1,11 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction
+
+fun main(args: Array<String>) {
+    println(" Max value from max function = ${max(4,7)}")
+    println("Max value from nrwMax function = ${newMax(3,5)}")
+}
+fun max(a : Int, b:Int): Int{
+    val maxValue = if (a > b)a else b
+    return maxValue
+}
+fun newMax(a: Int, b:Int): Int = if (a > b) a else b
