0cf86c163a02948e61591e097fd395ad2593ef42
Nuraulia Octaviana <nurauliaaa12@gmail.com>
2024-09-12 22:19:50 +0700

    Pemeriksaan Kondisi Null

---
+++ src/main/kotlin/PemeriksaanKondisiNull.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //conditions
+    var mayNumber : Int? = 15
+    if (mayNumber != null)
+        println(mayNumber *2)
+    else
+        println(" The enterted value isn't a number")
+}
